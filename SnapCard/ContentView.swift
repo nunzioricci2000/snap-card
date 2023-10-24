0b65ff9614e14039b357ba431e07cee4bdfff465
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-10-25 01:24:47 +0200

    Xcode project created

---
+++ SnapCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapCard
+//
+//  Created by Nunzio Ricci on 25/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapCard/SnapCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCardApp.swift
+//  SnapCard
+//
+//  Created by Nunzio Ricci on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
